55eaeb42877dbf006390aa6c3261990086cb92c7
Danny Seara <dsseara@gmail.com>
2017-04-12 18:09:34 -0400

    pca components finalized

--- analysis/getPCAComponents.m
+++ analysis/getPCAComponents.m
@@ -5,30 +5,36 @@
 % Chosen ones for 560_lessthan_0.6tc(20170411T131818268)
 chosenOnes = [3,4,5,6,7,11,13,15,20,22,25,34,35,42,45,48,49];
 
-%thetaMat = struct([]);
+pcaData = struct([]);
 
 for jj = chosenOnes
     figure
-    % [~,ind] = ismember(jj,chosenOnes);
-    % thetaMat(ind).filament = num2str(jj);
+    [~,ind] = ismember(jj,chosenOnes);
+    pcaData(ind).filament = num2str(jj);
 
     allTheta = Filament(jj).theta;
     
     nFrames = size(allTheta,2);
     ntheta = cellfun(@numel, allTheta);
     mean_size = round(mean(ntheta));
-    thetaMat = zeros(nFrames, mean_size);
+    matrix = zeros(nFrames, mean_size);
     for ii = 1:nFrames
         theta = allTheta{ii};
         % Interpolate the theta vector to be the same size as the mean theta vector
         interpedTheta = interp1(1:ntheta(ii), theta, 1: (ntheta(ii)-1)/(mean_size-1) :ntheta(ii));
-        thetaMat(ii,:) = interpedTheta; 
+        matrix(ii,:) = interpedTheta; 
     end
-    [coeff,score,latent,tsquared,explained,mu] = pca(thetaMat);
+    pcaData(ind).tangents = matrix;
+    [pcaData(ind).coeff,pcaData(ind).score,pcaData(ind).latent,...
+        pcaData(ind).tsquared,pcaData(ind).explained,pcaData(ind).mu]...
+        = pca(pcaData(ind).tangents);
 
-    colorline(score(:,1), score(:,2), 1:nFrames);
+    colorline(pcaData(ind).score(:,1), pcaData(ind).score(:,2), 1:nFrames);
     title(['Filament ', num2str(jj)]);
     xlabel('PCA component 1')
-    ylabel('PCA component 2)')
+    ylabel('PCA component 2')
 
+    saveas(gcf,['pcaTimeSeries_fil_',num2str(jj)],'fig')
+    saveas(gcf,['pcaTimeSeries_fil_',num2str(jj)],'tif')
+    saveas(gcf,['pcaTimeSeries_fil_',num2str(jj)],'epsc')
 end
