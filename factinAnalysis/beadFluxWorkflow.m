61fe15d62ae5f72b9a03d43fdb132337ea74e96e
Danny Seara <dsseara@gmail.com>
2017-04-26 12:05:09 -0400

    created probabilityFlux that takes any time series and produces a probability map and flux field

---
+++ factinAnalysis/beadFluxWorkflow.m
@@ -0,0 +1,56 @@
+% Script for getting the flux field for bead displacments with average
+% displacement subtracted off
+clear, close all;
+
+path = '/Volumes/Storage/Danny/122311_5ulRED_2pt0uMact_phall_beads_nta_nochol_mc_1to300xlink_2/adding6pt2ulunspunskmuscmyo_5s/msd/';
+nbins = 25;
+dt = 5;
+rg_cutoff = [0,Inf];
+tc = 50; % only look at time < 2/3 tc for now
+
+
+[probMap, fluxField, xEdges,yEdges] = beadMotionFluxLoop(path,nbins,dt,rg_cutoff,tc);
+
+dx.pre = xEdges.pre(2) - xEdges.pre(1);
+dy.pre = yEdges.pre(2) - yEdges.pre(1);
+binCentersX.pre = xEdges.pre(1:end-1)+dx.pre/2;
+binCentersY.pre = yEdges.pre(1:end-1)+dy.pre/2;
+
+netCurl.pre = sum(sum(curl(binCentersX.pre, binCentersY.pre, fluxField.pre(:,:,1), fluxField.pre(:,:,2))))*dx.pre*dy.pre;
+
+figure,
+pcolor(xEdges.pre(1:end-1), yEdges.pre(1:end-1), probMap.pre)
+colorbar;
+hold on;
+
+quiver(binCentersX.pre, binCentersY.pre, fluxField.pre(:,:,1), fluxField.pre(:,:,2),'w', 'LineWidth', 1.5);
+xlabel('\Deltax (\mum)')
+ylabel('\Deltay (\mum)')
+title(['Active systems, net curl=', num2str(netCurl.pre)])
+
+% Do the control on a system without myosin
+path = '/Volumes/Storage/Danny/122311_5ulRED_2pt0uMact_phall_beads_nta_nochol_mc_1to300xlink_2/tc1_5s/msd/';
+nbins = 25;
+dt = 5;
+rg_cutoff = [0,Inf];
+tc = []; 
+
+
+[probMap.ctrl, fluxField.ctrl, xEdges.ctrl,yEdges.ctrl] = beadMotionFluxLoop(path,nbins,dt,rg_cutoff,tc);
+
+dx.ctrl = xEdges.ctrl(2) - xEdges.ctrl(1);
+dy.ctrl = yEdges.ctrl(2) - yEdges.ctrl(1);
+binCentersX.ctrl = xEdges.ctrl(1:end-1)+dx.ctrl/2;
+binCentersY.ctrl = yEdges.ctrl(1:end-1)+dy.ctrl/2;
+
+netCurl.ctrl = sum(sum(curl(binCentersX.ctrl, binCentersY.ctrl, fluxField.ctrl(:,:,1), fluxField.ctrl(:,:,2))))*dx.ctrl*dy.ctrl;
+
+figure,
+pcolor(xEdges.ctrl(1:end-1), yEdges.ctrl(1:end-1), probMap.ctrl)
+colorbar;
+hold on;
+
+quiver(binCentersX.ctrl, binCentersY.ctrl, fluxField.ctrl(:,:,1), fluxField.ctrl(:,:,2),'w', 'LineWidth', 1.5);
+xlabel('\Deltax (\mum)')
+ylabel('\Deltay (\mum)')
+title(['Control, net curl=', num2str(netCurl.ctrl)])
